5fd1e84883a4f4fd70d715dfe1a186c856ce2062
Abbey Outain <abbeyoutain@outlook.com>
2021-11-24 23:13:13 -0600

    fruit loops 1 & 2

---
+++ csf1Homework/FruitLoops.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csf1Homework
+{
+    class FruitLoops
+    {
+        static void Main(string[] args)
+        {
+            //Fruit Loops 1
+
+            Console.WriteLine("Fruit Loops");
+
+            string[] fruit = { "apple",
+                               "apricot",
+                               "peach",
+                               "plum",
+                               "pear",
+                               "pomegranate" };
+
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.WriteLine(fruit[i]);
+            }//end for
+
+            //Fruit Loops 2
+
+            for (int i = 0; i < fruit.Length; i++)
+            {
+                Console.Write(fruit[i]);
+                for (int nbrLoops = 0; nbrLoops < i; nbrLoops++)
+                {
+                    Console.Write(".");
+                }
+                Console.WriteLine();
+            }//end for
+
+        }//end SVM
+    }//end class
+}//end namespace
